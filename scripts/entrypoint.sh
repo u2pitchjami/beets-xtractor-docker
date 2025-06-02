797b3cd9cdd7eb5be678021a876eb623ec086036
u2pitchjami <u2pitchjami@gmail.com>
2025-06-02 17:24:18 +0200

    end

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,11 +5,11 @@ CONFIG_DIR="/app/config"
 DEFAULT_CONFIG="/app/default_config"
 
 # Crée un dossier temporaire avec la config par défaut dans l'image
-if [ ! -d "$CONFIG_DIR" ] || [ -z "$(ls -A $CONFIG_DIR)" ]; then
-    echo "Config folder is empty, copying default config..."
-    mkdir -p "$CONFIG_DIR"
-    cp -r $DEFAULT_CONFIG/* $CONFIG_DIR/
-fi
+# if [ ! -d "$CONFIG_DIR" ] || [ -z "$(ls -A $CONFIG_DIR)" ]; then
+#     echo "Config folder is empty, copying default config..."
+#     mkdir -p "$CONFIG_DIR"
+#     cp -r $DEFAULT_CONFIG/* $CONFIG_DIR/
+# fi
 
 # Environment setup if needed
 #export PATH="/usr/local/bin:$PATH"
